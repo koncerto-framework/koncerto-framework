169d7472a0c97a8b831d66c094e82c2bba25145b
koncerto <koncerto-framework@protonmail.com>
2025-09-17 19:53:41 +0200

    Response

---
+++ koncerto.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Koncerto Framework
+ * This is Koncerto Framework main file
+ */
+
+class Koncerto
+{
+    /**
+     * @return string
+     */
+    public static function response() {
+        return '';
+    }
+}
